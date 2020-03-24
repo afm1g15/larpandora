7da1fbb5c1830a3ccc44027f66ed74e3ae13913c
Andrew Smith <andrewsmithweb@gmail.com>
2020-03-24 13:02:59 -0500

    Added FHiCL flag to enable or diable real data checks in PFParticle monitoring module
    cherrypick for mcc9 mc

--- larpandora/LArPandoraInterface/LArPandora.cxx
+++ larpandora/LArPandoraInterface/LArPandora.cxx
@@ -184,7 +184,7 @@ void LArPandora::CreatePandoraInput(art::Event &evt, IdToHitMap &idToHitMap)
 
     LArPandoraHelper::CollectHits(evt, m_hitfinderModuleLabel, artHits);
 
-    if (m_enableMCParticles && !evt.isRealData())
+    if (m_enableMCParticles && (m_disableRealDataCheck || !evt.isRealData()))
     {
         LArPandoraHelper::CollectMCParticles(evt, m_geantModuleLabel, artMCParticleVector);
 
@@ -214,7 +214,8 @@ void LArPandora::CreatePandoraInput(art::Event &evt, IdToHitMap &idToHitMap)
 
     LArPandoraInput::CreatePandoraHits2D(m_inputSettings, m_driftVolumeMap, artHits, idToHitMap);
 
-    if (m_enableMCParticles && !evt.isRealData())
+
+    if (m_enableMCParticles && (m_disableRealDataCheck || !evt.isRealData()))
     {
         LArPandoraInput::CreatePandoraMCParticles(m_inputSettings, artMCTruthToMCParticles, artMCParticlesToMCTruth, generatorArtMCParticleVector);
         LArPandoraInput::CreatePandoraMCLinks2D(m_inputSettings, idToHitMap, artHitsToTrackIDEs);
